392ad2b88d9b7a1c47320bfde36716a2b7babbcf
Kamil Kmetyk <kamil.kmetyk@darkfox.pl>
2022-03-13 17:31:17 +0100

    Add colors helper.

---
+++ bin/helpers/colors.dart
@@ -0,0 +1,7 @@
+import 'package:nyxx/nyxx.dart';
+
+class Colors {
+  static final DiscordColor foxy = DiscordColor.fromHexString('#e67e22');
+  static final DiscordColor error = DiscordColor.fromHexString('#cf2b15');
+
+}
