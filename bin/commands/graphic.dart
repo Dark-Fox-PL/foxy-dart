8f4d377dd0dd95d725a831cf701ff4b9750d7906
Kamil Kmetyk <kamil.kmetyk@darkfox.pl>
2022-03-14 18:43:46 +0100

    Support for dog and cat apis.

--- bin/commands/graphic.dart
+++ bin/commands/graphic.dart
@@ -18,6 +18,8 @@ class Graphic extends FoxyCommands {
   Future<FoxyCommands> handle() async {
     switch (message.command) {
       case 'fox': return _processFoxCommand();
+      case 'dog': return _processDogCommand();
+      case 'cat': return _processCatCommand();
       default: return unrecognizableCommand();
     }
   }
@@ -27,6 +29,7 @@ class Graphic extends FoxyCommands {
     http.Response response = await http.get(Uri.parse(apiUrl));
 
     if ('' == response.body) {
+      printError();
       return this;
     }
 
@@ -43,6 +46,68 @@ class Graphic extends FoxyCommands {
         title: 'What a Fox!',
         image: responseJson['image'] ?? '',
         description: 'What a cute Fox, just for you!',
+      );
+    } catch (e) {
+      printError();
+      print(e);
+    }
+
+    return this;
+  }
+
+  Future<FoxyCommands> _processDogCommand() async {
+    String apiUrl = CommandsConfig.commands['dog']?['options']['url'] ?? 'https://dog.ceo/api/breeds/image/random';
+    http.Response response = await http.get(Uri.parse(apiUrl));
+
+    if ('' == response.body) {
+      printError();
+      return this;
+    }
+
+    final responseJson = jsonDecode(response.body);
+
+    if (responseJson is! Map<String, dynamic>) {
+      printError();
+      return this;
+    }
+
+    try {
+      embedReply(
+        color: DiscordColor.dartBlue,
+        title: 'What a cute doggo!',
+        image: responseJson['message'] ?? '',
+        description: 'What a cute Dog, just for you!',
+      );
+    } catch (e) {
+      printError();
+      print(e);
+    }
+
+    return this;
+  }
+
+  Future<FoxyCommands> _processCatCommand() async {
+    String apiUrl = CommandsConfig.commands['cat']?['options']['url'] ?? 'https://api.thecatapi.com/v1/images/search';
+    http.Response response = await http.get(Uri.parse(apiUrl));
+
+    if ('' == response.body) {
+      printError();
+      return this;
+    }
+
+    final responseJson = jsonDecode(response.body);
+
+    if (responseJson is! List<dynamic>) {
+      printError();
+      return this;
+    }
+
+    try {
+      embedReply(
+        color: DiscordColor.flutterBlue,
+        title: 'What a Cat!',
+        image: responseJson[0]?['url'] ?? '',
+        description: 'What a cute Cat, just for you!',
       );
     } catch (e) {
       printError();
